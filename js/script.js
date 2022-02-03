d177cb7417946506b84aff4f8677b9cbe7327fe0
farapet800 <farapet777@mail.ru>
2022-02-03 22:42:04 +0300

    while/for.

--- js/script.js
+++ js/script.js
@@ -68,7 +68,17 @@ function ex4()
     }
 }
 ex4();
+
 console.log('----------------------');
+
+function showName(){
+    console.log('Вася!')
+}
+setTimeout(showName, 0);
+console.log('Коля!');
+
+console.log('----------------------');
+
 
 
 
