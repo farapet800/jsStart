24833945190df1773daeceffb97e555b2f284c6f
farapet800 <farapet777@mail.ru>
2022-02-06 19:55:39 +0300

    strings

---
+++ js/script4.js
@@ -0,0 +1,16 @@
+'use strict'
+
+//Задача.Получить символ "н" строки:
+// let text = 'фрилансер';
+
+let text = 'фрилансер';
+console.log(text.slice(-4,-3));
+
+//Задача.Получить строку в верхнем регистре.
+let texT = 'фарапонов';
+console.log(texT.toUpperCase());
+
+
+//Задача.Получить подстроку "лан" из:
+let teXT = 'фрилансер';
+console.log(teXT.slice(3,6));
